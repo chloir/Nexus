c4a06d8aa062ed0a610d45e53dd399e785be2015
chloir <pic.preserve@gmail.com>
2019-07-13 17:24:25 +0900

    Camera Required
    Camera movement required

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float duration = 0.5f;
+    [SerializeField] private GameObject aimTarget;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = this.transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 target = player.transform.position + offset;
+        
+        this.transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * duration);
+        transform.LookAt(aimTarget.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [SerializeField] private float velocityMulti = 1;
+    [SerializeField] private float boostVelocity = 20;
+    [SerializeField] private GameObject aimTarget;
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovementSystem();
+        transform.LookAt(aimTarget.transform);
+    }
+
+    void MovementSystem()
+    {
+        var direction = new Vector3();
+        
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            _rigidbody.AddForce(Vector3.left * velocityMulti, ForceMode.VelocityChange);
+            direction = Vector3.left;
+        }
+
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            _rigidbody.AddForce(Vector3.right * velocityMulti, ForceMode.VelocityChange);
+            direction = Vector3.right;
+        }
+
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            _rigidbody.AddForce(Vector3.back * velocityMulti, ForceMode.VelocityChange);
+            direction = Vector3.back;
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            _rigidbody.AddForce(Vector3.forward * velocityMulti, ForceMode.VelocityChange);
+            direction = Vector3.forward;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space)) { _rigidbody.AddForce(direction * boostVelocity, ForceMode.Impulse); }
+    }
+}
