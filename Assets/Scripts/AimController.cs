f4e9f3ed5fa5b66c7d59a0b29b60b644e569efeb
chloir <pic.preserve@gmail.com>
2019-07-14 22:04:44 +0900

    Aiming Required
    Aiming system required.
    Reticle image barrack set.

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AimController : MonoBehaviour
 {
+    [SerializeField] private Image reticle;
+    [SerializeField] private Camera mainCamera;
     void Start()
     {
-        
+        //offset = transform.position - player.transform.position;
     }
 
     void Update()
     {
-        
+        reticle.transform.position = RectTransformUtility.WorldToScreenPoint(mainCamera, transform.position);
+
+//        var inputAxis = new Vector3(Input.GetAxis("Mouse Y") * sensitivity, Input.GetAxis("Mouse X") * sensitivity, 0);
+//
+//        var playerPos = player.transform.position;
+//
+//        transform.position = playerPos + offset;
+//        
+//        transform.RotateAround(playerPos, inputAxis, rotationangle);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float boostVelocity = 20;
     [SerializeField] private GameObject aimTarget;
     
+    [SerializeField] private float sensitivity = 0.1f;
+    [SerializeField] private float clampAngle = 60;
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -17,7 +20,23 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovementSystem();
+        AimingSystem();
         transform.LookAt(aimTarget.transform);
+    }
+
+    void AimingSystem()
+    {
+        //マウス移動量
+        var mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        //mouseX *= reverseX ? -1 : 1; //X回転方向逆転
+        var mouseY = Input.GetAxis("Mouse Y") * sensitivity * -1;
+        //mouseY *= reverseY ? -1 : 1; //Y回転方向逆転
+        //メイン照準回転
+        var nowRot = this.transform.localEulerAngles;
+        var newX = this.transform.localEulerAngles.x + mouseY;
+        newX -= newX > 180 ? 360 : 0;
+        newX = Mathf.Abs(newX) > clampAngle ? clampAngle * Mathf.Sign(newX) : newX;
+        this.transform.localEulerAngles = new Vector3(newX, nowRot.y + mouseX, 0);
     }
 
     void MovementSystem()
